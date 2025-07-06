b5d5de08f76e9c0a76ed0db89e0184710d141976
diego <diego.correa.cumbicus07@outlook.com>
2025-07-05 19:54:15 -0500

    Implementar el patrón de diseño Singleton, para solucionar el problema de que se borran los datos al refrezcar la página y guardarlos en memoria.

--- DesignPatterns/Repositories/MyVehiclesRepository.cs
+++ DesignPatterns/Repositories/MyVehiclesRepository.cs
@@ -8,26 +8,26 @@ namespace DesignPatterns.Repositories
 {
     public class MyVehiclesRepository : IVehicleRepository
     {
-        private readonly ICollection<Vehicle> _memoryCollection;
+        private readonly MemoryCollection _memoryCollection = MemoryCollection.Instance;
 
         public MyVehiclesRepository()
         {
-            _memoryCollection = new List<Vehicle>();
+          
         }
 
         public void AddVehicle(Vehicle vehicle)
         {
-            _memoryCollection.Add(vehicle);
+            _memoryCollection.Vehicles.Add(vehicle);
         }
 
         public Vehicle Find(string id)
         {
-           return  _memoryCollection.FirstOrDefault(v => v.ID.Equals(new Guid(id)));
+           return  _memoryCollection.Vehicles.FirstOrDefault(v => v.ID.Equals(new Guid(id)));
         }
 
         public ICollection<Vehicle> GetVehicles()
         {
-            return _memoryCollection;
+            return _memoryCollection.Vehicles;
         }
 
         
